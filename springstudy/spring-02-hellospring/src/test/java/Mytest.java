56e2a176121cfbdeb8d56a5a69a6d7e5b5a914f7
cz <209059391@qq.com>
2024-09-04 21:14:41 +0800

    first init

--- springstudy/spring-02-hellospring/src/test/java/Mytest.java
+++ springstudy/spring-02-hellospring/src/test/java/Mytest.java
@@ -7,6 +7,7 @@ public class Mytest {
     public static void main(String[] args) {
         //获取spring的上下文对象
         ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
+        //注意为什么ClassPathXmlApplicationContext返回的对象是ApplicationContext类型，而不是Hello类型
         //对象在spring中管理，通过getBean方法获取对象
         Hello hello = (Hello) context.getBean("hello");
         System.out.println(hello);

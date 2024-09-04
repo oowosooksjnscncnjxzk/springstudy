0eb0e5c6d21645194c4c459830ae249bc5c4bda1
cz <209059391@qq.com>
2024-09-04 21:00:47 +0800

    first init

---
+++ springstudy/spring-02-hellospring/src/test/java/Mytest.java
@@ -0,0 +1,15 @@
+import com.kuang.pojo.Hello;
+import javafx.application.Application;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Mytest {
+    public static void main(String[] args) {
+        //获取spring的上下文对象
+        ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
+        //对象在spring中管理，通过getBean方法获取对象
+        Hello hello = (Hello) context.getBean("hello");
+        System.out.println(hello);
+
+    }
+}

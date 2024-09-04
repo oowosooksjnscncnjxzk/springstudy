8618088886e86b1852be0bfddd79afe5deacb41f
cz <209059391@qq.com>
2024-09-04 11:31:16 +0800

    first init

--- springstudy/spring-01-ioc/src/main/java/com/kuang/services/UserServiceImpl.java
+++ springstudy/spring-01-ioc/src/main/java/com/kuang/services/UserServiceImpl.java
@@ -3,10 +3,11 @@ package com.kuang.services;
 import com.kuang.dao.UseDao;
 import com.kuang.dao.UserDaoImpl;
 import com.kuang.dao.UserDaoMysqlImpl;
+import com.kuang.dao.UserDaoOracleImpl;
 
 public class UserServiceImpl implements UserService {
     //private UseDao userDao=new UserDaoImpl();
-    private UseDao userDao=new UserDaoMysqlImpl();
+    private UseDao userDao=new UserDaoOracleImpl();
     @Override
     public void getUser() {
         userDao.getUser();
